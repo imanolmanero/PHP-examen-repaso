ca5617241dd5227d145014e20482c25257a78afc
Imanol Manero <imanolmanero@gmail.com>
2025-11-05 00:20:33 +0100

    a

---
+++ mvc-plantilla-base/controllers/XxxxxController.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/BaseController.php';
+require_once __DIR__ . '/../models/XxxxxModel.php';
+
+class EmpleadoController extends BaseController {
+    
+    public function index() {
+        
+    }
+    
+    public function show() {
+        
+    }
+    
+    public function store() {
+        
+    }
+    
+    public function destroy() {
+        
+    }
+    
+    public function destroyAll() {
+        
+    }
+}
